7b387c74349e73cff6b19cf4c5bbdb1481a47d3d
celtas <celtasp@gmail.com>
2018-10-12 22:11:04 +0900

    Feat: Utilの拡張メソッド追加

--- Assets/PotatoTimer/Scripts/Util.cs
+++ Assets/PotatoTimer/Scripts/Util.cs
@@ -1,10 +1,23 @@
-﻿using UnityEngine.Events;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
 
-public static class Util{
-	
-	public static void InvokeSafe( this UnityEvent action) {
-		if (action != null)
-			action.Invoke();
-	}
-	
+public static class Util {
+    public static void InvokeSafe(this UnityEvent action) {
+        if (action != null)
+            action.Invoke();
+    }
+
+    // targetに近い値を返す
+    public static float NearestValue(this IEnumerable<float> list, float target) {
+        var min = list.Min( c => Mathf.Abs( c - target ) );
+        var o = list.First(c => Mathf.Abs(c - target) == min);
+        return o;
+    }
+
+    // 自身を含めない子要素のコンポーネントを取得
+    public static T[] GetComponentsInChildrenWithoutSelf<T>(this GameObject self) where T : Component {
+        return self.GetComponentsInChildren<T>().Where(c => self != c.gameObject).ToArray();
+    }
 }
