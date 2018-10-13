8b4be68bf81e534fd2d60067c60b3061908bd9fc
celtas <celtasp@gmail.com>
2018-10-13 21:46:15 +0900

    Feat: DrumRollUI - 時間を管理する機能の追加

---
+++ Assets/PotatoTimer/Scripts/TimePicker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class TimePicker : MonoBehaviour {
+	public DrumScrollRect hourDrum, minuteDrum, secondDrum;
+
+	public void setTime(int hour,int minute,int second) {
+		hourDrum.SelectedContentText = hour.ToString();
+		minuteDrum.SelectedContentText = minute.ToString();
+		secondDrum.SelectedContentText = second.ToString();
+	}
+	
+	public void setTime(int seconds) {
+		int hour = seconds / 3600;
+		int minute = (seconds / 3600) / 60;
+		int second = seconds % 60;
+		hourDrum.SelectedContentText = hour.ToString();
+		minuteDrum.SelectedContentText = minute.ToString();
+		secondDrum.SelectedContentText = second.ToString();
+	}
+	
+	public void setHour(int hour) {
+		hourDrum.SelectedContentText = hour.ToString();
+	}
+	
+	public void setMinute(int minute) {
+		minuteDrum.SelectedContentText = minute.ToString();
+	}
+	
+	public void setSecond(int second) {
+		secondDrum.SelectedContentText = second.ToString();
+	}
+	
+	public String getHour() {
+		return hourDrum.SelectedContentText;
+	}
+	
+	public String getMinute() {
+		return minuteDrum.SelectedContentText;
+	}
+	
+	public String getSecond() {
+		return minuteDrum.SelectedContentText;
+	}
+} 
